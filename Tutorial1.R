237af530ac12273ed3ead2df8f31ae6e28e13226
Tobias Schnabel <ctobiasschnabel@gmail.com>
2023-01-18 11:24:53 +0100

    finished tasks for first tutorial session

--- Tutorial1.R
+++ Tutorial1.R
@@ -41,6 +41,7 @@ stargazer(model1, type = 'text')
 ##6##
 #flat prior would make sense, inflation is unpredictable
 
+##7##
 #####PREP STAN#####
 library(tidyverse)
 library(recipes)
@@ -75,7 +76,46 @@ mod1_data <- list(
 # mod1_data$scale_beta <- apply(X, 2, sd) * sd(y) * 2.5
 mod1_data$loc_sigma <- sd(y)
 
-
+##8##
 mod1_fit = sampling(mod1, data = mod1_data, 
                     warmup = 500, iter = 1000, chains = 1, cores = 1, thin = 1)
 summary(mod1_fit)
+
+##9##
+trunc = round(0.8 * nrow(dat))
+dat.trunc = dat[-(trunc:nrow(dat)),]
+
+#regression model
+model2 = lm(AnnualCPIchange ~ date, data = dat.trunc)
+
+#results
+stargazer(model2, type = 'text')
+
+#make recipe
+rec2 = recipe(AnnualCPIchange ~ date, data = dat.trunc) %>% 
+  prep(retain = T)
+
+X2 = juice(rec2, all_predictors(), composition = 'matrix')
+y2 = drop(juice(rec2, all_outcomes(), composition = 'matrix'))
+
+#initialize
+mod2 = stan_model('tut1.stan')
+
+#prep data
+mod2_data <- list(
+  X = X2,
+  K = ncol(X2),
+  N = nrow(X2),
+  y = y2,
+  use_y_rep = FALSE,
+  use_log_lik = FALSE
+)
+
+# mod1_data$scale_alpha <- sd(y) * 10
+# mod1_data$scale_beta <- apply(X, 2, sd) * sd(y) * 2.5
+mod2_data$loc_sigma <- sd(y2)
+
+##8##
+mod2_fit = sampling(mod2, data = mod2_data, 
+                    warmup = 500, iter = 1000, chains = 1, cores = 1, thin = 1)
+summary(mod2_fit)
