c6f2ae191d74476762dccfe2306b3a42fde48322
Tobias Schnabel <ctobiasschnabel@gmail.com>
2023-01-26 22:46:15 +0100

    built out LOOCV comparison matrix and kable export

--- Tables.R
+++ Tables.R
@@ -33,4 +33,20 @@ kable(as.data.frame(summary(strong.fit.s2)), format = "latex",
       caption = "Fit with Strong Priors, Subset 2") %>% 
   save_kable("strongfit_sub2.tex")
 
+#LOOCV comparison full data
+#prep matrix
+loocvmat = as.matrix(cbind(rbind(strong.fit.s1$loo, strong.fit.s2$loo, strong.fit$loo, flat.fit$loo)[, 3:6],
+                     rbind(nrow(strong.fit.s1$data), nrow(strong.fit.s2$data), 
+                           nrow(strong.fit$data), nrow(flat.fit$data))))#loocv.comp
+rownames(loocvmat) = c("Strong Priors, 1k obs", "Strong Priors, 5k obs", 
+                       "Strong Priors, Full Data Set", "Flat Priors")
+colnames(loocvmat) = c("ELPD k-fold Estimate", "ELPD k-fold SE",
+                       "Eff. # Params Estimate", "Eff. # Params SE",
+                       "N")
+
+kable(loocvmat, format = "latex", 
+      digits = 4,
+      caption = "LOOCV Comparison") %>% 
+  save_kable("loocv_comp.tex")
+
 setwd('/Users/ts/Git/ise')
