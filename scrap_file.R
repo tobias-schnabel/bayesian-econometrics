693ba17eb9fa9f869f9160311841b50a282a14c1
Tobias Schnabel <ctobiasschnabel@gmail.com>
2023-01-28 12:48:36 +0100

    cleaned up scrap file

--- scrap_file.R
+++ scrap_file.R
@@ -1,32 +1,53 @@
-#storage for code that might need to be re-used
+## Tobias Schnabel ##
+## i6255807 ##
+
+rm(list = ls(all = TRUE)) #CLEAR ALL
+
+####Housekeeping####
+packages <- c("tidyverse", "broom", "ggpubr", "stargazer", "caret", "rstan",
+              "rstanarm", "kableExtra", "bayesplot", "coda", "ISLR")
+
+#Comment in lines below to Install packages not yet installed
+# installed_packages <- packages %in% rownames(installed.packages())
+# if (any(installed_packages == FALSE)) {
+#   install.packages(packages[!installed_packages])
+# }
+
+#load packages
+invisible(lapply(packages, library, character.only = TRUE))
+
+#set options
+options(mc.cores = parallel::detectCores())
+rstan_options(auto_write = TRUE)
+
+#load Credit Card Default Data Set
+attach(Default)
+
+#record start time
+start.time = Sys.time()
+
+# tidy
+#make factors numerical
+data = Default %>% 
+  mutate(default=ifelse(default=="No", 0,1)) %>% 
+  mutate(student=ifelse(student=="No", 0,1))
+
+#df summary statistics
+stargazer(data, type = "text")
 #prepare data for STAN
 #make recipe
 rec = recipe(default ~ student + balance + income, data = data) %>% 
   prep(retain = T)
 
-#extract X matrix and y vectors
+#extract X matrix and y vector
 X = juice(rec, all_predictors(), composition = 'matrix')
 y = juice(rec, all_outcomes(), composition = 'matrix') %>% drop()
 
-#make recipe #2 without income due to high correlation
-rec2 = recipe(default ~ student + balance, data = data) %>% 
-  prep(retain = T)
-X2 = juice(rec2, student, balance)
-# y is identical
 #feed data into STAN
-stan_data_1 <- list(
+stan_data <- list(
   X = X,
   K = ncol(X),
   N = nrow(X),
-  y = y,
-  use_y_rep = T,
-  use_log_lik = F
-)
-
-stan_data_2 <- list(
-  X = X2,
-  K = ncol(X2),
-  N = nrow(X2),
   y = y,
   use_y_rep = T,
   use_log_lik = F
@@ -37,35 +58,23 @@ stan.mod = stan_model('Final_Assignment_Schnabel.stan')
 
 #flat priors WITHOUT income variable
 # does not work
-# flat.fit.hmc = sampling(stan.mod, data = stan_data_2,
+# flat.fit.hmc = sampling(stan.mod, data = stan_data,
 #                     algorithm = "HMC",
 #                  warmup = 1000, iter = 10000, chains = 1, thin = 1)
 
-#does work
-flat.fit.fixedparam = sampling(stan.mod, data = stan_data_2, 
+#does work, but does not perform MCMC
+flat.fit.fixedparam = sampling(stan.mod, data = stan_data, 
                                algorithm = "Fixed_param",
                                warmup = 1000, iter = 10000, chains = 4, thin = 1)
 
 #does not work
-# flat.fit.nuts = sampling(stan.mod, data = stan_data_2, 
+# flat.fit.nuts = sampling(stan.mod, data = stan_data, 
 #                       algorithm = "NUTS",
 #                       warmup = 1000, iter = 10000, chains = 1, thin = 1)
 
 
-
-
-
-
-
-dot = ggtitle("Density Overlay Plot, 1000 Draws")
-do = ppc_dens_overlay(y, yrep.flat) + 
-  scale_x_continuous( limits=c(0, 1), 
-                      breaks = c(0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1)) +
-  dot
-
-ppc_ecdf_overlay(y, yrep.flat, discrete = T)
-
-
+fixedparam_params = rstan::extract(flat.fit.fixedparam)
+monitor(flat.fit.fixedparam)
 
 #check proportions of 0s and ones
 ppc_stat(y, yrep.flat, stat = "prop_zero", binwidth = 0.00005)
@@ -73,17 +82,17 @@ ppc_stat(y, yrep.flat, stat = "prop_one", binwidth = 0.00005)
 
 #check posterior.flat trace
 color_scheme_set("mix-blue-pink")
-mcmc_trace(flat.fit)
-mcmc_pairs(flat.fit)
+mcmc_trace(flat.fit.fixedparam)
+mcmc_pairs(flat.fit.fixedparam)
 
 # mcmc diagnostics
 # rhat
-plot(flat.fit, "rhat")
-plot(flat.fit, "rhat_hist")
+plot(flat.fit.fixedparam, "rhat")
+plot(flat.fit.fixedparam, "rhat_hist")
 # ratio of effective sample size to total posterior.flat sample size
-plot(flat.fit, "neff")
-plot(flat.fit, "neff_hist")
+plot(flat.fit.fixedparam, "neff")
+plot(flat.fit.fixedparam, "neff_hist")
 # autocorrelation by chain
-plot(flat.fit, "acf", pars = "(Intercept)")
-plot(flat.fit, "acf_bar", pars = "(Intercept)")
-mcmc_acf(flat.fit)
+plot(flat.fit.fixedparam, "acf", pars = "(Intercept)")
+plot(flat.fit.fixedparam, "acf_bar", pars = "(Intercept)")
+mcmc_acf(flat.fit.fixedparam)
