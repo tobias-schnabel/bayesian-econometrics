85f8136329ae6543e942be283ab52c82eff2c752
Tobias Schnabel <ctobiasschnabel@gmail.com>
2023-01-31 00:44:10 +0100

    unified number of shown digits and fixed alignment for kable tables

--- Tables.R
+++ Tables.R
@@ -13,7 +13,7 @@ stargazer(baseline, title = "Baseline Estimation Results", float = T,
 
 #Data Integrity Table
 kable(data_integrity, format = "latex", 
-      digits = 4,
+      align = 'c',
       caption = "Data Splits") %>% 
   save_kable("data_integrity.tex")
 
@@ -31,13 +31,13 @@ kable(as.data.frame(summary(strong.fit)), format = "latex",
   save_kable("strongfit_full.tex")
 
 kable(as.data.frame(summary(strong.fit.s1)), format = "latex", 
-      digits = 4,
+      align = 'c',
       caption = "Fit with Strong Priors, Subset 1") %>% 
   kable_styling(latex_options = "HOLD_position") %>% 
   save_kable("strongfit_sub1.tex")
 
 kable(as.data.frame(summary(strong.fit.s2)), format = "latex", 
-      digits = 4,
+      align = 'c',
       caption = "Fit with Strong Priors, Subset 2") %>% 
   kable_styling(latex_options = "HOLD_position") %>% 
   save_kable("strongfit_sub2.tex")
@@ -48,13 +48,21 @@ loocvmat = as.matrix(cbind(rbind(strong.fit.s1$loo, strong.fit.s2$loo, strong.fi
                      rbind(nrow(strong.fit.s1$data), nrow(strong.fit.s2$data), 
                            nrow(strong.fit$data), nrow(flat.fit$data))))#loocv.comp
 rownames(loocvmat) = c("Strong Priors, 1k obs", "Strong Priors, 5k obs", 
-                       "Strong Priors, Full Data Set", "Flat Priors")
-colnames(loocvmat) = c("ELPD k-fold Estimate", "ELPD k-fold SE",
-                       "Eff. # Params Estimate", "Eff. # Params SE",
+                       "Strong Priors, 10k obs", "Flat Priors")
+colnames(loocvmat) = c("ELPD", "ELPD SE",
+                       "# Params", "# Params SE",
                        "N")
 
-kable(loocvmat, format = "latex", 
-      digits = 4,
+loo_df = as.data.frame(loocvmat)
+#make columns numeric while preserving colanames
+loo_df$`ELPD` = as.numeric(loo_df$`ELPD`)
+loo_df$`ELPD SE` = as.numeric(loo_df$`ELPD SE`)
+loo_df$`# Params` = as.numeric(loo_df$`# Params`)
+loo_df$`# Params SE` = as.numeric(loo_df$`# Params SE`)
+loo_df$N = as.numeric(loo_df$N)
+
+kable(loo_df, format = "latex", 
+      align = 'c',
       caption = "10-fold CV Comparison") %>% 
   kable_styling(latex_options = "HOLD_position") %>% 
   save_kable("loocv_comp.tex")
